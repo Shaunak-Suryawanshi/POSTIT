21c42ffecbcc9d9626d9454cc67fd94f49935dc5
shaunakk <callmeshaun@users.noreply.github.com>
2026-02-14 14:55:09 +0530

    RAILWAY

--- src/main/java/com/postit/config/SecurityConfig.java
+++ src/main/java/com/postit/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
+                                "/error",
                                 "/",
                                 "/index.html",
                                 "/static/**",
